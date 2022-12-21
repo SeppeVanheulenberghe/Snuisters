9aeee864905f11766572663c969f937d4e91be20
SeppeVanheulenberghe <120470500+SeppeVanheulenberghe@users.noreply.github.com>
2022-12-21 13:35:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ inventory_name = "Bestelbon.xlsx"
 inventory = Inventory(inventory_name, images_filepath)
 template_name = inventory.details.template_name
 inventory_dict = inventory.get_inventory_dict
-# inventory = Inventory(inventory_name, images_filepath).get_inventory_dict
 
 # write purchase sheet
 purchase_sheet = Purchase_Sheet(inventory_dict)
