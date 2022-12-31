bb39e0097d7b6367ca12d0d54e532c2f06473c98
SeppeVanheulenberghe <120470500+SeppeVanheulenberghe@users.noreply.github.com>
2022-12-31 13:24:13 +0100

    Adapt main to new composition structure

--- main.py
+++ main.py
@@ -2,40 +2,25 @@ from tkinter import *
 import customtkinter
 from packages.inventory import Inventory
 from packages.document_factory import read_creator
-from GUI.GUI_Code import EntryBoxHandler
-
-
-def get_details_from_GUI(ENTRY_BOXES: dict[str, customtkinter.CTkEntry]) -> tuple[str, tuple[str, str, str]]:
-    """Get the inventory details from the GUI."""
-    entry_box = EntryBoxHandler(ENTRY_BOXES)
-    INVENTORY_NAME = entry_box.GetInventoryName
-    PURCHASE_SHEET_NAME = entry_box.GetPurchaseSheetName
-    HOST = entry_box.GetHostName
-    TEMPLATE_NAME = entry_box.GetTemplateName
-    DETAILS = (HOST, TEMPLATE_NAME, PURCHASE_SHEET_NAME)
-    return INVENTORY_NAME, DETAILS
-
-
-def get_inventory_name_from_GUI(ENTRY_BOXES: dict[str, customtkinter.CTkEntry]):
-    """Get inventory name from GUI."""
-    entry_box = EntryBoxHandler(ENTRY_BOXES)
-    return entry_box.GetInventoryName
-
-
-def create_inventory_from_GUI(ENTRY_BOXES: dict[str, customtkinter.CTkEntry]):
-    """Inventory creation from GUI codec."""
-    INVENTORY_NAME, DETAILS = get_details_from_GUI(ENTRY_BOXES)
-    inventory = Inventory(INVENTORY_NAME)
-    inventory.set_details = DETAILS
-    return inventory
+from GUI.GUI_Code import EntryBoxHandler, GUI
 
 
 def create_document_from_GUI(ENTRY_BOXES: dict[str, customtkinter.CTkEntry]):
     """Main document creation codec."""
+    # prepare GUI
+    gui = GUI(ENTRY_BOXES)
+    gui.get_gui_info()
+
+    # get info
+    INVENTORY_NAME = gui.InventoryName
+    PURCHASE_SHEET_NAME = gui.PurchaseSheetName
+    HOST_NAME = gui.HostName
+    TEMPLATE_NAME = gui.TemplateName
+    DETAILS = (HOST_NAME, TEMPLATE_NAME, PURCHASE_SHEET_NAME)
+
     # create inventory
-    inventory = create_inventory_from_GUI(ENTRY_BOXES)
-    PURCHASE_SHEET_NAME = inventory.details.purchase_sheet_name
-    TEMPLATE_NAME = inventory.details.template_name
+    inventory = Inventory(INVENTORY_NAME)
+    inventory.set_details = DETAILS
 
     # get document factory
     fac = read_creator("Snuisters Purchase Sheet")
